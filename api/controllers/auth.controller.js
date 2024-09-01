a0380939a1ba15b48c44ef14a146c0aff884ce34
Joonho Kim <lippoint.surf0622@gmail.com>
2024-09-01 11:19:31 +0900

    set cookie test

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,11 +5,8 @@ export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    // HASH THE PASSWORD
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    // CREATE A NEW USER AND SAVE TO DB
-    const newUser = await prisma.user.create({
+    await prisma.user.create({
       data: {
         email,
         password: hashedPassword,
@@ -17,17 +14,42 @@ export const register = async (req, res) => {
       },
     });
 
-    console.log(newUser);
-
     res.status(201).json({ message: 'User created successfully' });
   } catch (err) {
-    console.log(err);
     res.status(500).json({ message: 'Failed to create user!' });
   }
 };
 
-export const login = (req, res) => {
-  // db operations
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) return res.status(400).json({ message: 'Invalid Credentials!' });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(400).json({ message: 'Invalid Credentials!' });
+
+    // const { ...userInfo } = user;
+    // GENERATE COOKIE TOKEN AND SEND TO USER
+    // res.setHeader('Set-Cookie', 'test=' + 'myValue').json('success');
+    const age = 1000 * 60 * 60 * 24 * 7;
+    res
+      .cookie('test2', 'myValue2', {
+        httpOnly: true,
+        // secure: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: 'Login Successful!' });
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to login!' });
+  }
 };
 
 export const logout = (req, res) => {
