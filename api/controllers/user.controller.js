31e862e3ea917814a73ef177b2b705033253071d
Joonho Kim <lippoint.surf0622@gmail.com>
2024-09-01 20:47:21 +0900

    Add user API logic on the server side

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,74 @@
+import prisma from '../lib/prisma.js';
+import bcrypt from 'bcrypt';
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ messsage: 'Failed to get users!' });
+  }
+};
+
+export const getUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ messsage: 'Failed to get user!' });
+  }
+};
+
+export const updatetUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+  const { password, avatar, ...inputs } = req.body;
+
+  if (id !== tokenUserId) {
+    return res.status(403).json({ message: 'Not Authorized!' });
+  }
+  try {
+    let updatedPassword = null;
+    if (password) {
+      updatedPassword = await bcrypt.hash(password, 10);
+    }
+
+    const updateUser = await prisma.user.update({
+      where: { id },
+      data: {
+        ...inputs,
+        ...(updatedPassword && { password: updatedPassword }),
+        ...(avatar && { avatar }),
+      },
+    });
+    res.status(200).json(updateUser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ messsage: 'Failed to update users!' });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+
+  if (id !== tokenUserId) {
+    return res.status(403).json({ message: 'Not Authorized!' });
+  }
+
+  try {
+    await prisma.user.delete({
+      where: { id },
+    });
+    res.status(200).json({ message: 'User deleted!' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ messsage: 'Failed to delete users!' });
+  }
+};
