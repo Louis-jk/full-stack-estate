d2f2396731af4047986a0bd965e6660ecd128471
Joonho Kim <lippoint.surf0622@gmail.com>
2024-09-01 17:28:32 +0900

    Handle client-side data based on user information

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import './login.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import apiRequest from '../../lib/apiRequest';
+import { AuthContext } from '../../context/AuthContext';
 
 function Login() {
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
+
+  const { updateUser } = useContext(AuthContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,7 +26,7 @@ function Login() {
         password,
       });
 
-      localStorage.setItem('user', JSON.stringify(res.data));
+      updateUser(res.data);
       navigate('/');
     } catch (err) {
       console.log(err);
