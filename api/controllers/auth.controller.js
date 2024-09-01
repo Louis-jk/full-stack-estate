dddde54be40190f887654012ab703b7cb98c9137
Joonho Kim <lippoint.surf0622@gmail.com>
2024-09-01 10:19:39 +0900

    apply prisma client to auto api register

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,29 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  // HASH THE PASSWORD
+  try {
+    // HASH THE PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  const hashPassword = await bcrypt.hash(password, 10);
-  console.log(hashPassword);
+    // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        username,
+      },
+    });
 
-  // CREATE A NEW USER AND SAVE TO DB
+    console.log(newUser);
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Failed to create user!' });
+  }
 };
 
 export const login = (req, res) => {
